4d5725da193931225f55fe629dbfcc4397f47119
lilunze <3737@winnermedical.com>
2022-06-10 20:20:59 +0800

    1231231

--- config/config.js
+++ config/config.js
@@ -1,1 +1,24 @@
+/*
+ * @Author: lilunze
+ * @LastEditors: lilunze
+ */
 exports.keys = '123456';
+
+// config.sqlite = {
+//     default: {
+//         path: ':memory:',
+//         options: null
+//     },
+//     // 单实例
+//     client: {
+//         path: ':memory:',
+//         options: null    
+//     },
+//     // 多实例
+//     clients: {
+//        db1: {
+//           path: ':memory:',
+//           options: null   
+//        },
+//     }
+// };
